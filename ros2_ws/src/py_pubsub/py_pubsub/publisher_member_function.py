4f257f15decae6b62b615a502e4596ce00872c23
Pravokotnik <uhan.masa@gmail.com>
2025-03-06 01:06:52 +0100

    final

--- ros2_ws/src/py_pubsub/py_pubsub/publisher_member_function.py
+++ ros2_ws/src/py_pubsub/py_pubsub/publisher_member_function.py
@@ -17,21 +17,25 @@ from rclpy.node import Node
 
 from std_msgs.msg import String
 
+from tutorial_interfaces.msg import CustomMessage
+
 
 class MinimalPublisher(Node):
 
     def __init__(self):
         super().__init__('minimal_publisher')
-        self.publisher_ = self.create_publisher(String, 'topic', 10)
+        self.publisher_ = self.create_publisher(CustomMessage, 'custom_topic', 10)
         timer_period = 0.5  # seconds
         self.timer = self.create_timer(timer_period, self.timer_callback)
         self.i = 0
 
     def timer_callback(self):
-        msg = String()
-        msg.data = 'Hello World: %d' % self.i
+        msg = CustomMessage()
+        msg.text = f"Hello World: {self.i}"  # Set the string field
+        msg.number = self.i  # Set the integer field
+        msg.flag = (self.i % 2 == 0)  # Set the boolean field (alternates between True and False)
         self.publisher_.publish(msg)
-        self.get_logger().info('Publishing: "%s"' % msg.data)
+        self.get_logger().info(f'Publishing: "{msg.text}", {msg.number}, {msg.flag}')
         self.i += 1
 
 
